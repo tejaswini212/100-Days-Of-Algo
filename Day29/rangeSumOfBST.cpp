de7c6de3f8916bb4db302152f6bf2414af3e0490
tejaswini212 <tejaswini212@users.noreply.github.com>
2021-01-04 18:59:45 +0530

    added code

---
+++ Day29/rangeSumOfBST.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int ans = 0;
+        if(root==NULL)
+            return 0;
+        int leftSum = rangeSumBST(root->left,low,high);
+        
+        int rightSum = rangeSumBST(root->right,low,high);
+        if(root->val>=low && root->val<=high)
+            ans+=root->val;
+        return ans+leftSum+rightSum;
+    }
+};
